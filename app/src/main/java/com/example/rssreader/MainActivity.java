d49742829a2359df2a60ae63c1c8d7dc94ba24f3
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2015-08-24 19:37:27 +0900

    first commit

---
+++ app/src/main/java/com/example/rssreader/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.rssreader;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ListView mListView;
+    private ArrayAdapter<Integer> mListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        // 0 ~ 29 の数字のリストを作成
+        List<Integer> numberList = new ArrayList<>();
+
+        for (int i = 0 ; i < 30 ; i++) {
+            numberList.add(i);
+        }
+        // ここまで
+
+        mListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numberList);
+
+        mListView.setAdapter(mListAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), position + "番目がタップされました", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
