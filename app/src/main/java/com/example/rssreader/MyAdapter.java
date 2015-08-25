2fcabf7f35f9e466a73ff185529ae52786af39d8
KeithYokoma <Keith.Yokoma@gmail.com>
2015-08-25 14:41:27 +0900

    Goal!

--- app/src/main/java/com/example/rssreader/MyAdapter.java
+++ app/src/main/java/com/example/rssreader/MyAdapter.java
@@ -1,9 +1,30 @@
 package com.example.rssreader;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * TODO ArrayAdapter を継承して、独自の Adapter をつくろう
  * @author KeishinYokomaku
  */
-public class MyAdapter {
-	public static final String TAG = MyAdapter.class.getSimpleName();
+public class MyAdapter extends ArrayAdapter<Person> {
+	public MyAdapter(Context context, List<Person> objects) {
+		super(context, R.layout.list_item_person, R.id.text1, objects);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = super.getView(position, convertView, parent);
+		TextView text1 = (TextView) view.findViewById(R.id.text1);
+		TextView text2 = (TextView) view.findViewById(R.id.text2);
+		Person person = getItem(position);
+		text1.setText(person.getName() + "(" + person.getAge() + ")");
+		text2.setText(person.getComment());
+		return view;
+	}
 }
