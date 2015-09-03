a6cffef0250cf5177fdb44ff56d38e66efebab09
punchdrunker <takafumi.nanao@mixi.co.jp>
2015-09-03 19:04:33 +0900

    ひとまず通信してみる

--- app/src/main/java/com/example/rssreader/FeedFetcher.java
+++ app/src/main/java/com/example/rssreader/FeedFetcher.java
@@ -2,6 +2,11 @@ package com.example.rssreader;
 
 import android.content.Context;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +25,22 @@ public class FeedFetcher {
      * @return
      */
     public List<FeedItem> fetch() {
+        String result = null;
+        Request request = new Request.Builder()
+                .url("http://news.google.co.jp/news?cf=all&hl=ja&pz=1&ned=jp&topic=h&num=3&output=rss")
+                .get()
+                .build();
+        OkHttpClient client = new OkHttpClient();
+
         try {
-            // ネットワーク遅延をエミュレート
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
+            Response response = client.newCall(request).execute();
+            result = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        List<FeedItem> result = new ArrayList<>();
-        result.add(new FeedItem("最初の大ニュースです！", "これはすごい", "スクープ", "2015/9/1"));
-        result.add(new FeedItem("次の大ニュースです！", "これはすごいこれはすごい", "スクープ", "2015/9/1"));
-        return result;
+        List<FeedItem> items = new ArrayList<>();
+        items.add(new FeedItem("最初の大ニュースです！", "これはすごい", "スクープ", "2015/9/1"));
+        items.add(new FeedItem("次の大ニュースです！", "これはすごいこれはすごい", "スクープ", "2015/9/1"));
+        return items;
     }
 }
