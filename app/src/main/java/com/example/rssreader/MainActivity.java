633cc8752b829846dd07e689d2b1280f8d60b597
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-10-14 21:22:46 +0900

    タイトルとメニューの設定

--- app/src/main/java/com/example/rssreader/MainActivity.java
+++ app/src/main/java/com/example/rssreader/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // TODO: タイトルを設定してみる
-        // TODO: メニューを表示する
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu_main);
 
         mListView = (ListView) findViewById(R.id.listView);
 
