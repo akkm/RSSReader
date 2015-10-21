0b0b1ead6134b679f5c0a4ef54b9196f64be9dd3
Atsuto Yamada <jayuiyong@gmail.com>
2015-10-21 22:29:49 +0900

    lesson25start

---
+++ app/src/main/java/com/example/rssreader/lesson25/Lesson25Activity.java
@@ -0,0 +1,41 @@
+package com.example.rssreader.lesson25;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rssreader.R;
+
+public class Lesson25Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson25);
+
+        // TODO: [1] onCreate Log
+
+
+        // TODO: [2]savedInstanceState からデータを復元しよう
+
+    }
+
+    // TODO: [1] onStart
+
+
+    // TODO: [1] onResume
+
+
+    // TODO: [1] onRestart
+
+
+    // TODO: [1] onPause
+
+
+    // TODO: [1] onStop
+
+
+    // TODO: [1] onDestroy
+
+
+    // TODO: [2] onSaveInstanceState でデータを格納しよう
+}
---
+++ app/src/main/java/com/example/rssreader/lesson25/LessonItem.java
@@ -0,0 +1,7 @@
+package com.example.rssreader.lesson25;
+
+/**
+ * Created by akkuma on 2015/10/21.
+ */
+public class LessonItem {
+}
